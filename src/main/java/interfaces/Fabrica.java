542190a84b5b8c36c24a6ffcbb81d0802ecf718c
aguscorvo <aguscorvo@gmail.com>
2020-09-11 21:19:15 -0300

    Terminé caso 'Consulta de programa de formación'

--- src/main/java/interfaces/Fabrica.java
+++ src/main/java/interfaces/Fabrica.java
@@ -56,27 +56,14 @@ public class Fabrica {
 	public IControladorAgregarCursoAPF getIControladorAgregarCursoAPF() {
 		return new ControladorAgregarCursoAPF();
 	}
-	/*
-	public IControladorAltaCurso getIControladorAltaCurso() {
-		return new ControladorAltaCurso();
-	}
-	public IControladorAltaEdicion getIControladorAltaEdicion() {
-		return new ControladorAltaEdicion();
-	}
 	
-	public IControladorAltaProgFormacion getIControladorAltaProgFormacion() {
-		return new ControladorAltaProgFormacion();
-	}
-	
-	public IControladorConsultaCurso getIControladorConsultaCurso() {
-		return new ControladorConsultaCurso();
-	}
-	public IControladorConsultaEdicion getIControladorConsultaEdicion() {
-		return new ControladorConsultaEdicion();
-	}
 	public IControladorConsultaProgFormacion getIControladorConsultaProgFormacion() {
-		return new ControladorProgFormacion();
+		return new ControladorConsultaProgFormacion();
 	}
+	
+	
+	/*
+	
 	
 	public IControladorConsultaUsuario getIControladorConsultaUsuario() {
 		return new ControladorConsultaUsuario();
