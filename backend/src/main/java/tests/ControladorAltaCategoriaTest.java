b864eb712d80429739a7a2d5951a089bf991ca6a
aguscorvo <aguscorvo@gmail.com>
2020-11-17 14:08:10 -0300

    Actualización de testing en AltaCategoria.

--- backend/src/main/java/tests/ControladorAltaCategoriaTest.java
+++ backend/src/main/java/tests/ControladorAltaCategoriaTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -8,6 +10,8 @@ import org.junit.runners.MethodSorters;
 import excepciones.CategoriaRepetidaException;
 import interfaces.Fabrica;
 import interfaces.IControladorAltaCategoria;
+import logica.Categoria;
+import logica.ManejadorCategoria;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ControladorAltaCategoriaTest {
@@ -19,16 +23,15 @@ public class ControladorAltaCategoriaTest {
 	
 	@Test
 	public void Test_1_ingresarCategoriaOK() throws CategoriaRepetidaException   {		
-		System.out.println("ingresar categoria OK");
-
-		iCon.ingresarCategoria("Categoria");	
+		iCon.ingresarCategoria("Categoria");
+		ManejadorCategoria mC = ManejadorCategoria.getInstancia();
+		Categoria aux= mC.getCategoria("Categoria");
+		assertNotNull(aux);
 	}
 
 	@Test(expected = CategoriaRepetidaException.class)
-	public void Test_2_ingresarCategoriaERROR() throws CategoriaRepetidaException {		
-
+	public void Test_2_ingresarCategoriaERROR() throws CategoriaRepetidaException {	
 			iCon.ingresarCategoria("Categoria");
-
 	}
 	
 	
