73c896d0332d8b64920053fa8d41c0004176df64
aguscorvo <aguscorvo@gmail.com>
2020-11-18 17:16:28 -0300

    Testing de Consulta de Usuario en un 99.6%

--- backend/src/main/java/tests/AllTests.java
+++ backend/src/main/java/tests/AllTests.java
@@ -4,12 +4,15 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import logica.ControladorConsultaEdicion;
+
 @RunWith(Suite.class)
 @SuiteClasses({  ControladorSeguirUsuariosTest.class,ControladorAgregarCursoAPFTest.class, ControladorInscripcionAProgramaTest.class, 
 	ControladorAltaProgFormacionTest.class,ControladorSeleccionarEstudiantesTest.class, ControladorListarResultadosInscripcionesTest.class, 
 	ControladorAltaCursoTest.class, ControladorModificarDatosUsuarioTest.class, ControladorAltaInstitutoTest.class, 
 	ControladorAltaCategoriaTest.class, ControladorConsultarProgFormacionTest.class, ControladorAltaUsuarioTest.class, 
-	ControladorIniciarSesionTest.class, ControladorAltaEdicionTest.class, ControladorBuscarCursoOProgramaTest.class})
+	ControladorIniciarSesionTest.class, ControladorAltaEdicionTest.class, ControladorBuscarCursoOProgramaTest.class, 
+	ControladorConsultaUsuarioTest.class})
 public class AllTests {
 
 }
